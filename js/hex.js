a33db7cf861e9d984da65316afc4c5f1e375884e
MartinsHenrique <martisnhenrique00@gmail.com>
2026-08-27 23:53:54 -0300

    caveira

--- js/hex.js
+++ js/hex.js
@@ -9,13 +9,17 @@ export class hex
     
     receberData(data,x,y,onClick)
     {
+        this.corPerigo = "#777777";
         this.data = data;
         this.element = this.criarElemento();
         this.configurarClique(onClick);
         this.posicionar(x,y)
         this.verificarTerreno();
-
+        this.verificarPerigo();
+        
+        this.desselecionar();
     }
+    
     configurarClique(onClick)
     {
         this.element.addEventListener("click", () =>
@@ -108,8 +112,8 @@ export class hex
     }
     desselecionar()
     {
-        this.pintarBorda("black");
-        this.element.setAttribute("stroke-width", "1");
+        this.pintarBorda(this.corPerigo);
+        this.element.setAttribute("stroke-width", "5");
 
         this.limparInfo();
     }
@@ -175,6 +179,62 @@ export class hex
             case "montanha":
                 this.pintarHex("#954535");
                 break;
+        }
+    }
+
+    verificarPerigo()
+    {
+        switch (this.data.perigo)
+        {
+            case "seguro":
+                this.corPerigo = "green";
+                this.polygon.setAttribute("stroke-dasharray", "none");
+                break;
+
+            case "inseguro":
+                this.corPerigo = "#E0B52F";
+                this.criarCaveiras(1);
+                break;
+
+            case "arriscado":
+                this.corPerigo = "#E87518";
+                this.criarCaveiras(2);
+                break;
+
+            case "mortal":
+                this.corPerigo = "#D9362B";
+                this.criarCaveiras(3);
+                break;
+        }
+
+        
+        this.pintarBorda(this.corPerigo);
+
+        if (this.data.perigo !== "seguro")
+        {
+            this.polygon.setAttribute("stroke-dasharray","6 3");
+        }
+    }
+
+    criarCaveiras(quantidade)
+    {
+        const tamanho = Math.min(this.largura,this.altura) * 0.16;
+        const espacamento = tamanho * 0.15;
+        const larguraTotal = quantidade * tamanho + (quantidade - 1) * espacamento;
+        const inicioX = (this.largura - larguraTotal) / 2;
+        const y = this.altura * 0.02;
+
+        for (let i = 0; i < quantidade; i++)
+        {
+            const caveira = document.createElementNS("http://www.w3.org/2000/svg","image");
+
+            caveira.setAttribute("href", "./images/caveira.svg");
+            caveira.setAttribute("width", tamanho);
+            caveira.setAttribute("height", tamanho);
+            caveira.setAttribute("x",inicioX + i * (tamanho + espacamento));
+            caveira.setAttribute("y", y);
+
+            this.element.appendChild(caveira);
         }
     }
 }
